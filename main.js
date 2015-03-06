87dbcdb3929f0cb2a9f31cbae72e4e45aa4ec7f8
micwinter <michele_winter@brown.edu>
2015-03-05 20:50:57 -0500

    added timbre

--- main.js
+++ main.js
@@ -6,7 +6,6 @@
 function clicked(){
 
 	var poem = $("#mypoem").val();
-	console.log(poem);
 
 	$.ajax({
 		url:"https://text-sentiment.p.mashape.com/analyze",
@@ -16,15 +15,25 @@ function clicked(){
 			"Content-Type": "application/x-www-form-urlencoded",
 			"X-Mashape-Key": "a6VxOx7M3Amshij6uyj84fTrVHmjp1TSOldjsnWL91sN5PtGUV"
 		},
-		data: {text: poem},
+		sentimentVal: {text: poem},
 		dataType:"json"
-	}).done(function(data) {
+	}).done(function(sentimentVal) {
 		console.log("Response:");
-		console.log(data);
+		console.log(sentimentVal);
+
+		var negative = sentimentVal.neg;
+		var positive = sentimentVal.pos;
+		var middle = sentimentVal.mid;
+
+
+
 	});
 }
 
 $(document).ready(function() {
+
+	T("sin").play();
+
 	$("#submit").click(function() {
 		clicked();
 	});
